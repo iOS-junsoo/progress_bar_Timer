ecf1c7e945c007a57f80028bca960505783f44bc
kimjunsoo <rlawnstn01023@naver.com>
2021-10-04 15:55:14 +0900

    프로그레스바 타이머 만들기

--- progress_bar_Timer/ViewController.swift
+++ progress_bar_Timer/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
 
+    @IBOutlet weak var progressBar: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.progressTintColor = .red
+        progressBar.progress = 0.0
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        var progress: Float = 0.0
+        progressBar.progress = progress
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(timer) in
+            
+            progress += 0.01
+            self.progressBar.progress = progress
+            
+            if self.progressBar.progress == 1.0 {
+                self.progressBar.progress = 0.0
+            }
+            
+        })
+    }
 }
 
